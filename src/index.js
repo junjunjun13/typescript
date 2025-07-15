79d478474c2c201585df8a21ff05f92023831ac5
junjunjun13 <>
2025-07-15 16:05:55 +0900

    타입스크립트 과제

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const userName = "정동준";
+const age = 26;
+const isStudent = true;
+const nothing = null;
+let anything = "타입스크립트 과제 중입니다";
+console.log(userName, age, isStudent, nothing, anything);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const userName: string = "정동준";
+const age: number = 26;
+const isStudent: boolean = true;
+const nothing: null = null;
+let anything: any = "타입스크립트 과제 중입니다";
+
+console.log(name, age, isStudent, nothing, anything);
